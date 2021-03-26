b6375acc7867d5f50a77b788a9951dd7f5694b4f
yohanmishkin <yohanmishkin@protonmail.com>
2021-03-25 20:15:33 -0400

    Tiny styling

--- src/ui/CourseCard.js
+++ src/ui/CourseCard.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 export default function CourseCard({ course, isToggling, toggleFavourite }) {
   return (
-    <button data-test={`course-${course.id}`} onClick={() => toggleFavourite(course)}>
+    <button className="course-card" data-test={`course-${course.id}`} onClick={() => toggleFavourite(course)}>
       <img
         src={course.instructor_image_url}
         alt={`Headshot of ${course.instructor_name}`}
         data-test={`instructor-headshot-${course.id}`}
+        height="130"
       />
       <p data-test={`course-instructor-${course.id}`}>{course.instructor_name}</p>
       <p data-test={`course-title-${course.id}`}>{course.title}</p>
       {isToggling ? (
         <span className="toggling">&#9862;</span>
-      ) : course.favourite ? (
+      ) : course.favorite ? (
         <span className="favourite">&#9733;</span>
       ) : (
         <span className="not-yet-favourited">&#9734;</span>
